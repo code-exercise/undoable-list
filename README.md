46898c65e6cff433bfcf6d24f469a66dd95acbb5
Kunal Singh <kunal.s.rathor@gmail.com>
2019-06-24 22:39:23 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # undoable-list
-Undable list
+Undoable list
+
+
+Your List should provide following API -
+
+```
+public interface ListIntf<T> extends Iterable<T>{
+
+    void add(T value);
+    void remove(T value);
+    void undo(); // We can redo last operation on list
+    void redo(); // We can undo last operation on list
+    
+}
+```
---
+++ src/com/exercise/undoablelist/UnDoableList.java
@@ -0,0 +1,29 @@
+package com.exercise.undoablelist;
+
+import java.util.Iterator;
+
+public class UnDoableList implements ListIntf<String> {
+
+
+    @Override
+    public Iterator<String> iterator() {
+        return null;
+    }
+
+    @Override
+    public void add(String value) {
+
+    }
+
+    @Override
+    public void remove(String value) {
+    }
+
+    @Override
+    public void undo() {
+    }
+
+    @Override
+    public void redo() {
+    }
+}
